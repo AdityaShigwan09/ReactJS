96e90a65ddff8b1485b4d0359d2d6d8624d8ee7f
AdityaShigwan09 <adityashigwan035@gmail.com>
2026-01-09 23:49:37 +0530

    Created the Components/Example.js

---
+++ src/Components/Example.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function Hello({name}){
+    return(
+    <div>
+      <h1>Hello React JS</h1>
+      <p>Lets Get Started {name}!!!!</p>
+    </div>
+    );
+}
+
+export default Hello;
